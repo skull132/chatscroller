0d55e9920db806e820fdff588c412dece71e9fe0
skull132 <erki.meinberg@gmail.com>
2017-01-21 15:17:44 +0200

    Update, 21JAN2017 Reworks the tab and context system a little, making it possible to add each REGEX and other properties to each of context. Adds the chat mark-up used ingame currently and makes it context sensitive. Implements the mixed tab. Develops the HTML interface a little.

--- index.js
+++ index.js
@@ -1,86 +1,145 @@
-var Chat = function(bindTo) {
-  var el = bindTo;
-  var messages = [];
-  var tabs = [
-    {OOC: true},
-    {admin: true, LOOC: true, 'admin fight': true},
-    {GOOC: true}
-  ];
-  var maxMessages = 50000;
-  var cullMessages = 1000;
-  var tabMax = 1000;
-
-  var currentTab = 0;
-  var currentSize = 0;
-
-  // check if a tab should show this message
-  function messageMatches(tab, message) {
-    for (var i = 0; i < message.context.length; i++) {
-      if (tab[message.context[i]]) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  // turn message into html
-  function nodeFromMessage(message) {
-    var text = message.context.join(' | ') + ': ' + message.content;
-
-    var div = document.createElement('div');
-    div.appendChild(document.createTextNode(text));
-    return div;
-  }
-
-  function displayMessage(message) {
-    el.appendChild(nodeFromMessage(message));
-    currentSize += 1;
-
-    if (currentSize > tabMax) {
-      el.removeChild(el.firstChild);
-      currentSize -= 1;
-    }
-  }
-
-
-  // public functions
-
-  return {
-    addMessage: function addMessage(message) {
-      var parsed = JSON.parse(message);
-      messages.push(parsed);
-      if (messageMatches(tabs[currentTab], parsed)) {
-        displayMessage(parsed);
-      }
-      if (messages.length > maxMessages) {
-        messages = messages.slice(cullMessages);
-      }
-    },
-
-    swapTab: function swapTab(tab) {
-      currentTab = tab;
-      currentSize = 0;
-
-      // build an array of messages for the new tab
-      var curMessage = messages.length - 1;
-      var backwardMessages = [];
-      while (curMessage >= 0 && currentSize < tabMax) {
-        if (messageMatches(tabs[currentTab], messages[curMessage])) {
-          backwardMessages.push(messages[curMessage]);
-          currentSize += 1;
+var Chat = function (bindTo) {
+    var el = bindTo;
+    var messages = [];
+    var contexts = {
+        OOC: {
+            accepted_tags: [
+                [/((\W|^)\*)([^*]*)(\*(\W|$))/, "<b>", "</b>"],
+                [/((\W|^)\/)([^\/]*)(\/(\W|$))/, "<i>", "</i>"],
+                [/((\W|^)~)([^~]*)(~(\W|$))/, "<strike>", "</strike>"],
+                [/((\W|^)_)([^_]*)(_(\W|$))/, "<u>", "</u>"]
+            ]
+        },
+        LOOC: {
+            accepted_tags: [
+                [/((\W|^)\*)([^*]*)(\*(\W|$))/, "<b>", "</b>"],
+                [/((\W|^)\/)([^\/]*)(\/(\W|$))/, "<i>", "</i>"],
+                [/((\W|^)~)([^~]*)(~(\W|$))/, "<strike>", "</strike>"],
+                [/((\W|^)_)([^_]*)(_(\W|$))/, "<u>", "</u>"]
+            ]
+        },
+        IC: {
+            accepted_tags: [
+                [/((\W|^)\*)([^*]*)(\*(\W|$))/, "<b>", "</b>"],
+                [/((\W|^)\/)([^\/]*)(\/(\W|$))/, "<i>", "</i>"]
+            ]
+        },
+        LOGS: {
+            accepted_tags: []
+        },
+        MSAY: {
+            accepted_tags: []
+        },
+        ASAY: {
+            accepted_tags: []
+        },
+        PMS: {
+            accepted_tags: []
         }
-        curMessage -= 1;
-      }
+    };
+    var tabs = [
+        // Global OOC tab
+        {OOC: contexts.OOC, MSAY: contexts.MSAY, ASAY: contexts.ASAY, PMS: contexts.PMS},
+        // IC tab
+        {IC: contexts.IC, LOOC: contexts.LOOC, PMS: contexts.PMS},
+        // Admin tab
+        {LOGS: contexts.LOGS},
+        // Custom tab
+        {}
+    ];
+    var maxMessages = 50000;
+    var cullMessages = 1000;
+    var tabMax = 1000;
 
-      // build a new div and swap it with our current one
-      var old = el;
-      el = el.cloneNode(false);
+    var currentTab = tabs[0];
+    var currentSize = 0;
 
-      for (var i = backwardMessages.length - 1; i >= 0; i--) {
-        el.appendChild(nodeFromMessage(backwardMessages[i]));
-      }
-
-      old.parentNode.replaceChild(el, old);
+    // check if a tab should show this message
+    function messageMatches(tab, message) {
+        if (tab[message.context]) {
+            return true;
+        }
+        return false;
     }
-  };
-}
+
+    // turn message into html
+    function nodeFromMessage(message) {
+        var text = message.context + ': ' + parseMarkup(message, currentTab);
+
+        var div = document.createElement('div');
+        div.innerHTML = text;
+        return div;
+    }
+
+    function displayMessage(message) {
+        var node = nodeFromMessage(message);
+        el.appendChild(node);
+        node.scrollIntoView();
+        currentSize += 1;
+
+        if (currentSize > tabMax) {
+            el.removeChild(el.firstChild);
+            currentSize -= 1;
+        }
+    }
+
+    function parseMarkup(message, tab) {
+        var msg = message.content;
+        for (var i = 0; i < tab[message.context].accepted_tags.length; i++) {
+            msg = msg.replace(tab[message.context].accepted_tags[i][0], "$2" + tab[message.context].accepted_tags[i][1] + "$3" + tab[message.context].accepted_tags[i][2] + "$5");
+        }
+        return msg;
+    }
+
+    // public functions
+
+    return {
+        addMessage: function addMessage(message) {
+            var parsed = JSON.parse(message);
+            messages.push(parsed);
+            if (messageMatches(currentTab, parsed)) {
+                displayMessage(parsed);
+            }
+            if (messages.length > maxMessages) {
+                messages = messages.slice(cullMessages);
+            }
+        },
+
+        swapTab: function swapTab(tab) {
+            currentTab = tabs[tab];
+            currentSize = 0;
+
+            // build an array of messages for the new tab
+            var curMessage = messages.length - 1;
+            var backwardMessages = [];
+            while (curMessage >= 0 && currentSize < tabMax) {
+                if (messageMatches(currentTab, messages[curMessage])) {
+                    backwardMessages.push(messages[curMessage]);
+                    currentSize += 1;
+                }
+                curMessage -= 1;
+            }
+
+            // build a new div and swap it with our current one
+            var old = el;
+            el = el.cloneNode(false);
+
+            for (var i = backwardMessages.length - 1; i >= 0; i--) {
+                el.appendChild(nodeFromMessage(backwardMessages[i]));
+            }
+
+            old.parentNode.replaceChild(el, old);
+        },
+
+        toggleMixed: function toggleMixed(context) {
+            if (tabs[3].hasOwnProperty(context)) {
+                delete tabs[3][context];
+                document.getElementById(context).className = "";
+            }
+            else {
+                tabs[3][context] = contexts[context];
+                document.getElementById(context).className = "enabled";
+            }
+        }
+    };
+};
